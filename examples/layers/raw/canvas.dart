981857268820cc1934b049ee9a0b0061bc2ba8c2
Loic Sharma <sharma.loic@gmail.com>
2022-06-29 11:52:26 -0700

    Prototype

--- examples/layers/raw/canvas.dart
+++ examples/layers/raw/canvas.dart
@@ -5,100 +5,9 @@
 // This example shows how to use the ui.Canvas interface to draw various shapes
 // with gradients and transforms.
 
-import 'dart:math' as math;
+import 'dart:io';
 import 'dart:typed_data';
 import 'dart:ui' as ui;
-
-ui.Picture paint(ui.Rect paintBounds) {
-  // First we create a PictureRecorder to record the commands we're going to
-  // feed in the canvas. The PictureRecorder will eventually produce a Picture,
-  // which is an immutable record of those commands.
-  final ui.PictureRecorder recorder = ui.PictureRecorder();
-
-  // Next, we create a canvas from the recorder. The canvas is an interface
-  // which can receive drawing commands. The canvas interface is modeled after
-  // the SkCanvas interface from Skia. The paintBounds establishes a "cull rect"
-  // for the canvas, which lets the implementation discard any commands that
-  // are entirely outside this rectangle.
-  final ui.Canvas canvas = ui.Canvas(recorder, paintBounds);
-
-  final ui.Paint paint = ui.Paint();
-  canvas.drawPaint(ui.Paint()..color = const ui.Color(0xFFFFFFFF));
-
-  final ui.Size size = paintBounds.size;
-  final ui.Offset mid = size.center(ui.Offset.zero);
-  final double radius = size.shortestSide / 2.0;
-
-  final double devicePixelRatio = ui.window.devicePixelRatio;
-  final ui.Size logicalSize = ui.window.physicalSize / devicePixelRatio;
-
-  // Saves a copy of current transform onto the save stack
-  canvas.save();
-
-  // Note that transforms that occur after this point apply only to the
-  // yellow-bluish rectangle
-
-  // This line will cause the transform to shift entirely outside the paint
-  // boundaries, which will cause the canvas interface to discard its
-  // commands. Comment it out to see it on screen.
-  canvas.translate(-mid.dx / 2.0, logicalSize.height * 2.0);
-
-  // Clips the current transform
-  canvas.clipRect(
-    ui.Rect.fromLTRB(0, radius + 50, logicalSize.width, logicalSize.height),
-    clipOp: ui.ClipOp.difference,
-  );
-
-  // Shifts the coordinate space of and rotates the current transform
-  canvas.translate(mid.dx, mid.dy);
-  canvas.rotate(math.pi/4);
-
-  final ui.Gradient yellowBlue = ui.Gradient.linear(
-    ui.Offset(-radius, -radius),
-    ui.Offset.zero,
-    <ui.Color>[const ui.Color(0xFFFFFF00), const ui.Color(0xFF0000FF)],
-  );
-
-  // Draws a yellow-bluish rectangle
-  canvas.drawRect(
-    ui.Rect.fromLTRB(-radius, -radius, radius, radius),
-    ui.Paint()..shader = yellowBlue,
-  );
-
-  // Note that transforms that occur after this point apply only to the
-  // yellow circle
-
-  // Scale x and y by 0.5.
-  final Float64List scaleMatrix = Float64List.fromList(<double>[
-      0.5, 0.0, 0.0, 0.0,
-      0.0, 0.5, 0.0, 0.0,
-      0.0, 0.0, 1.0, 0.0,
-      0.0, 0.0, 0.0, 1.0,
-  ]);
-  canvas.transform(scaleMatrix);
-
-  // Sets paint to transparent yellow
-  paint.color = const ui.Color.fromARGB(128, 0, 255, 0);
-
-  // Draws a transparent yellow circle
-  canvas.drawCircle(ui.Offset.zero, radius, paint);
-
-  // Restores the transform from before `save` was called
-  canvas.restore();
-
-  // Sets paint to transparent red
-  paint.color = const ui.Color.fromARGB(128, 255, 0, 0);
-
-  // Note that this circle is drawn on top of the previous layer that contains
-  // the rectangle and smaller circle
-  canvas.drawCircle(const ui.Offset(150.0, 300.0), radius, paint);
-
-  // When we're done issuing painting commands, we end the recording an receive
-  // a Picture, which is an immutable record of the commands we've issued. You
-  // can draw a Picture into another canvas or include it as part of a
-  // composited scene.
-  return recorder.endRecording();
-}
 
 ui.Scene composite(ui.Picture picture, ui.Rect paintBounds) {
   final double devicePixelRatio = ui.window.devicePixelRatio;
@@ -114,11 +23,48 @@ ui.Scene composite(ui.Picture picture, ui.Rect paintBounds) {
   return sceneBuilder.build();
 }
 
+Future<void> savePicture(ui.Picture picture, ui.Rect paintBounds, String path) async {
+  final File file = File(path);
+  if (await file.exists()) {
+    return;
+  }
+
+  final ui.Image image = await picture.toImage(paintBounds.width.toInt(), paintBounds.height.toInt());
+  final ByteData? byteData = await image.toByteData(format: ui.ImageByteFormat.png);
+  final Uint8List pngBytes = byteData!.buffer.asUint8List();
+
+  await file.writeAsBytes(pngBytes);
+}
+
+int frame = 0;
 void beginFrame(Duration timeStamp) {
   final ui.Rect paintBounds = ui.Offset.zero & (ui.window.physicalSize / ui.window.devicePixelRatio);
-  final ui.Picture picture = paint(paintBounds);
+  final ui.PictureRecorder recorder = ui.PictureRecorder();
+  final ui.Canvas canvas = ui.Canvas(recorder, paintBounds);
+  final ui.ParagraphBuilder builder = ui.ParagraphBuilder(ui.ParagraphStyle(
+    fontStyle: ui.FontStyle.normal,
+    fontWeight: ui.FontWeight.normal,
+    fontSize: 15.0,
+  ));
+
+  frame++;
+  builder.addText('Hello world! $frame');
+
+  final ui.Paragraph paragraph = builder.build();
+  paragraph.layout(const ui.ParagraphConstraints(width: 100.0));
+  canvas.drawParagraph(paragraph, ui.Offset.zero);
+
+  final ui.Picture picture = recorder.endRecording();
+  savePicture(
+    picture,
+    paintBounds,
+    r'C:\Users\sharm\Downloads\hello_world_text.png',
+  );
+
   final ui.Scene scene = composite(picture, paintBounds);
   ui.window.render(scene);
+
+  ui.window.scheduleFrame();
 }
 
 void main() {
